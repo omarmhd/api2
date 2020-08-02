147c8e3df4897a6c4f67903ef80f8fe2d9b76a00
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-08-02 14:16:22 +0300

    first commit

--- config/app.php
+++ config/app.php
@@ -173,6 +173,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
+
 
     ],
 
