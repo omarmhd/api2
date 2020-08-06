6ae2134ea48a1237932047e53bcb0150af364625
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-08-06 21:25:28 +0300

    first commit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -105,8 +105,8 @@ class UserController extends Controller
 
         $validator = Validator::make($request->all(), [
 
-            'full_name' => 'sometimes|unique:users'  . ',id,' . $id,
-            'login_name' => 'sometimes|unique:users' . ',id,' . $id,
+            'full_name' => 'sometimes|unique:users',
+            'login_name' => 'sometimes|unique:users' ,
             'password' => 'required',
             'phone' => 'required|Numeric',
             'card'=>'required',
