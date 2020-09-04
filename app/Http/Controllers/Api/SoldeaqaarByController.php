0f5b8969f06ec61d9a6b612eb35f8cc60a5279b5
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-09-04 23:55:25 +0300

    first commit

--- app/Http/Controllers/Api/SoldeaqaarByController.php
+++ app/Http/Controllers/Api/SoldeaqaarByController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Eaqaar;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\SoldEaqaarResource;
 use App\SoldEaqaar;
 use App\soldeaqaar_by;
