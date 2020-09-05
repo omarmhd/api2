25e49bd97c39aabd6c8f6682a262a3ea14080148
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-09-05 17:00:00 +0300

    first commit

--- app/Http/Controllers/Api/SoldeaqaarByController.php
+++ app/Http/Controllers/Api/SoldeaqaarByController.php
@@ -251,7 +251,7 @@ class SoldeaqaarByController extends Controller
        ]);
 
 
-       $sold = SoldEaqaar::find($id);
+       $sold = SoldEaqaar::first($id);
 
        $eqaar = Eaqaar::find( $sold->eaqaar_id);
         $eqaar->state = $request->state;
