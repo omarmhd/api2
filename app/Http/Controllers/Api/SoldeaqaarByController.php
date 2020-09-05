bd66a72461843f069f0e1bee49e9fe50b3153e43
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-09-05 18:36:52 +0300

    first commit

--- app/Http/Controllers/Api/SoldeaqaarByController.php
+++ app/Http/Controllers/Api/SoldeaqaarByController.php
@@ -295,7 +295,7 @@ class SoldeaqaarByController extends Controller
 
         $receivable->delete();
     }
-       return SoldEaqaarResource::collection($sold_esqaar);
+    //   return SoldEaqaarResource::collection();
 
     }
 
