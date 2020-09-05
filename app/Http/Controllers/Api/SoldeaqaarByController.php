9d706d1851c67c24dffa82d2a0af83fc925675aa
omar al haddad <42090545+omarmhd@users.noreply.github.com>
2020-09-05 16:12:02 +0300

    first commit

--- app/Http/Controllers/Api/SoldeaqaarByController.php
+++ app/Http/Controllers/Api/SoldeaqaarByController.php
@@ -253,7 +253,7 @@ class SoldeaqaarByController extends Controller
 
        $sold = SoldEaqaar::orderBy('id', 'desc')->take(1)->get();
 
-return  response(['$sold->eaqaar_id'=> $sold_esqaar]);
+return  response(['$sold->eaqaar_id'=> $sold_esqaar->get()]);
        $eqaar = Eaqaar::find( $sold->eaqaar_id);
         $eqaar->state = $request->state;
         $eqaar->area = $request->area;
